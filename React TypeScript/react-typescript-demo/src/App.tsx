e8c52a98bad28230e2475c60fc577c8e35e413a8
Augustine97 <manu71997mathew@gmail.com>
2022-03-15 08:49:59 +0530

    6. Event Props

--- React TypeScript/react-typescript-demo/src/App.tsx
+++ React TypeScript/react-typescript-demo/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import { Button } from './components/Button';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
@@ -41,6 +43,12 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Leonardo !!</Heading>
       </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log('clicked', event, id);
+        }}
+      />
+      <Input handleChange={(event) => console.log(event)} value='' />
     </div>
   );
 }
