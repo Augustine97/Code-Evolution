030a7ea4b5a75f05d814196e7309af3f71e0d05a
Augustine97 <manu71997mathew@gmail.com>
2022-03-15 06:50:08 +0530

    4. Basic Props

--- React TypeScript/react-typescript-demo/src/App.tsx
+++ React TypeScript/react-typescript-demo/src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-
-import { Greet } from './components/Greet';
-
 import './App.css';
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  };
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne',
+    },
+    {
+      first: 'Clark',
+      last: 'Kent',
+    },
+    {
+      first: 'Princess',
+      last: 'Diana',
+    },
+  ];
   return (
-    <div className="App">
-     <Greet name='Vishwas'/>
+    <div className='App'>
+      <Greet name='Vishwas' messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
 }
