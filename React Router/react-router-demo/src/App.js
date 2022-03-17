2daa3eaf8eda3ed539da0b9a287a6f363aaf3e25
Augustine97 <manu71997mathew@gmail.com>
2022-03-18 02:17:39 +0530

    Router 9. Index Route

--- React Router/react-router-demo/src/App.js
+++ React Router/react-router-demo/src/App.js
@@ -2,9 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { About } from './components/About';
 import { FeaturedProducts } from './components/FeaturedProducts';
-import { NewProducts } from './components/NewProducts';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar';
+import { NewProducts } from './components/NewProducts';
 import { NoMatch } from './components/NoMatch';
 import { OrderSummary } from './components/OrderSummary';
 import { Products } from './components/Products';
@@ -19,6 +19,7 @@ function App() {
         <Route path='order-summary' element={<OrderSummary />} />
         <Route path='*' element={<NoMatch />} />
         <Route path='products' element={<Products />}>
+          <Route index element={<FeaturedProducts />} />
           <Route path='featured' element={<FeaturedProducts />} />
           <Route path='new' element={<NewProducts />} />
         </Route>
